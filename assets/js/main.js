f2d3167fcc08bb48f3f5f8ac3f701989dd2627e0
Musfique Rains <fahimvsrock@gmail.com>
2021-09-23 14:58:36 +0600

    initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,19 @@ $(document).ready(function() {
         slidesToScroll: 3,
         autoplay: true,
         autoplaySpeed: 2000,
+        responsive: [
+
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            }
+
+        ]
 
 
     });
